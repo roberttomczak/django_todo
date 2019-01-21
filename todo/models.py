b757140f7137492915eb0fb1872e44be603776f2
Robert Tomczak <ufo22@onet.eu>
2019-01-21 16:02:08 +0100

    Adding code to render todos
    Setting models for Todo, coding new temaplate and views

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import re
 
-# Create your models here.
+class Todo(models.Model):
+    todo_text = models.CharField(max_length=50)
+    created_at = models.DateTimeField('Created at', auto_now_add=True)
+
+    def __str__(self):
+        return self.todo_text
+
+    def is_todo_contain_words(self):
+        return True if re.match(r'[a-zA-Z]+\w', self.todo_text) else False
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Todo
+
+
+def index(request):
+    latest_todos = Todo.objects.order_by('-created_at')
+    #todo = get_object_or_404(Todo, pk=todo_id)
+    return render(request, 'todo/index.html', {'latest_todos': latest_todos})
