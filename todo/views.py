4a751df3acd2c524fa47986dc42653812f55699f
Robert Tomczak <ufo22@onet.eu>
2019-01-21 19:57:54 +0100

    Establishing connection with database Coded getting values from form and saving to database. Advance app templates.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-
+from django.shortcuts import render
 
 from .models import Todo
 
@@ -8,9 +6,8 @@ from .models import Todo
 def index(request):
     latest_todos = Todo.objects.order_by('-created_at')
     if request.method == "POST":
-            todo_form_value =request.POST.dict()['todo']
+            todo_form_value =request.POST.get('todo')
             todo = Todo(todo_text = todo_form_value)
             todo.save()
             
-    #todo = get_object_or_404(Todo, pk=todo_id)
     return render(request, 'todo/index.html', {'latest_todos': latest_todos})
