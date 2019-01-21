2fec92852cd060deedf5b87ff3580f8398e07c9e
Robert Tomczak <ufo22@onet.eu>
2019-01-21 18:18:53 +0100

    Adding forms and functionality behind it

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 from .models import Todo
 
 
 def index(request):
     latest_todos = Todo.objects.order_by('-created_at')
+    if request.method == "POST":
+            todo_form_value =request.POST.dict()['todo']
+            todo = Todo(todo_text = todo_form_value)
+            todo.save()
+            
     #todo = get_object_or_404(Todo, pk=todo_id)
     return render(request, 'todo/index.html', {'latest_todos': latest_todos})
